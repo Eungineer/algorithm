f488ad71b5ad6620ced98745af314355b0deab0c
Eungineer <wjddms_e@naver.com>
2023-01-09 23:28:45 +0900

    한수

---
+++ BaekJoon/함수/1065.cpp
@@ -0,0 +1,46 @@
+/*
+
+Jeongeun in charge
+Update: 2023.1.8
+
+
+문제
+어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오. 
+
+입력
+첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+
+출력
+첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int check(int n) {
+	int cnt = 0;
+	if (n < 100)
+		return n;
+	else {
+		cnt = 99;
+
+		for (int i = 100; i <= n; i++) {
+			int hun = i / 100;
+			int ten = (i % 100) / 10;
+			int one = i % 10;
+
+			if ((hun - ten) == (ten - one))
+				cnt++;
+		}
+	}
+	return cnt;
+}
+
+int main() {
+	int n;
+	cin >> n;
+    cout << check(n) << endl;
+
+    return 0;
+}
